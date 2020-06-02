b8a11f0e931568fb17777b8556cf034c38d3e57b
renato-milano <renatomilano96@libero.it>
2020-06-02 21:47:00 +0200

    Added Scoreboard

--- sketch.js
+++ sketch.js
@@ -15,11 +15,58 @@
     var oldy = 0;
     var foodToEat;
     var points=0;
+    var firebaseConfig = {
+        apiKey: "AIzaSyC5zcaRgWU5nMT5eF1S3dD6_TM4od0kTwU",
+        authDomain: "dbstupidgame.firebaseapp.com",
+        databaseURL: "https://dbstupidgame.firebaseio.com",
+        projectId: "dbstupidgame",
+        storageBucket: "dbstupidgame.appspot.com",
+        messagingSenderId: "181762538631",
+        appId: "1:181762538631:web:4769fca7f3c8820335cc22",
+        measurementId: "G-BLF1TEC8VD"
+      };
+
+
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+
+      var db = firebase.firestore();
+    
+      
+      let foo = prompt('Inserisci il tuo Nome');
+      var scores = [];
+      db.collection('scores').orderBy('Points','desc').limit(5).get().then((snapshot) =>{
+        snapshot.docs.forEach(doc =>{
+            scores.push(doc.data());
+
+
+            
+        });
+        var father = document.getElementById("banner");
+        var ToAdd= document.createElement("H1");
+            ToAdd.innerText="TOP 5:";
+            ToAdd.style.color ='red' ;
+            father.appendChild(ToAdd);
+        scores.forEach(element => {
+            ToAdd= document.createElement("P");
+            ToAdd.innerText= element.Name+" : " + element.Points;
+            ToAdd.style.fontSize='20px';
+            father.appendChild(ToAdd);
+    
+          });
+      });
+
+      console.log(scores);
+
+     
+
     function setup() {
     
     let cnv= createCanvas(displayWidth/2, displayHeight/2);
     cnv.id("mycanvas");
     strokeWeight(5);
+    
     
   }
 
@@ -72,7 +119,7 @@ function mouseStopped(){                                 // the actual function 
 */
 
 background(220);
-document.getElementById('pointShower').innerHTML= 'Points: '+points;
+
 if(true){
     var max=0.5;
     var min=-0.5;
@@ -96,6 +143,8 @@ if(true){
         
         foodToEat=new food(random(displayWidth/2),random(displayHeight/2));
         points++;
+        
+        document.getElementById('pointShower').innerHTML= 'Points: '+points;
         
         
     }
@@ -122,9 +171,11 @@ particles.forEach(element => {
     //console.log(DistanceX+" "+DistanceY);
     if( DistanceX <5  && DistanceX>-5 && DistanceY <5  && DistanceY>-5 && element.count>30 && points!=0){
 
+        saveScore(points);
         alert("Game Over || Points: "+ points);
         points=0;
         element.pushed=true;
+        document.getElementById('pointShower').innerHTML= 'Points: '+points;
         particles=[];
         
     }
@@ -134,9 +185,13 @@ if( (mouseX>displayWidth/2 ||
     mouseY>displayHeight/2 ||
     mouseX<0 ||
     mouseY<0) && points!=0){
-
+    
+        saveScore(points);
     alert("Game Over || Points: "+ points);
+    
     points=0;
+    document.getElementById('pointShower').innerHTML= 'Points: '+points;
+
     particles=[];
     
 }
@@ -307,4 +362,12 @@ for(var i = particles.length - 1; i >= 0; i--) {
         
     }
 
+}
+
+function saveScore(mpoints) {
+    var record={Points: mpoints, Name: foo};
+    console.log(record);
+
+    db.collection("scores").add(record);
+    
 }
